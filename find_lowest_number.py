364cdcbf93e95cfce4f648e1640ef9078e1b0daf
Moinuddinthe <103385838+Moinuddinthe@users.noreply.github.com>
2025-11-30 01:25:45 -0500

    Update find_lowest_number.py

--- find_lowest_number.py
+++ find_lowest_number.py
@@ -1,39 +1,23 @@
-# The script will find the lowest number in a file and write it to another file.
-#
-# Run as: python3 find_lowest_number.py <input_file> <output_file>
-#
-# Example: python3 find_lowest_number.py numbers.txt lowest_number.txt
-#
-# If python is setup to run as "python" instead of "python3" on the machine, 
-# then we should use "python" instead of "python3" in the above.
-#
-# The input file should contain one number per line. The output file will 
-# contain the lowest number.
-#
-# If the input file is blank, the output file will contain the text: "No 
-# numbers found in file".
+class TextAnalyzer:
+    def __init__(self, text):
+        self.text = text
 
-import sys
+    def word_count(self):
+        words = self.text.split()
+        return len(words)
 
-input_filename = sys.argv[1]
-output_filename = sys.argv[2]
 
-number_found = False
+class TextFileSaver:
+    def save(self, text, filename):
+        with open(filename, "w") as f:
+            f.write(text)
+        print(f"Text saved to {filename}")
 
-with open(input_filename, 'r') as input_file:
-    for line in input_file:
-        if number_found == False:
-            try:
-                lowest_number = float(line)
-                number_found = True
-            except ValueError:
-                break
-        else:
-            if float(line) < lowest_number:
-                lowest_number = float(line)
 
-with open(output_filename, 'w') as output_file:
-    if number_found:
-        output_file.write(str(lowest_number) + "\n")
-    else:
-        output_file.write("No numbers found in file\n")
+# Demo usage
+text = "An example that fixes SRP."
+analyzer = TextAnalyzer(text)
+print(f"Word count: {analyzer.word_count()}")
+
+saver = TextFileSaver()
+saver.save(text, "example.txt")
